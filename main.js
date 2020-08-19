f68385704abc87306448f227aca5323342ab7f9f
masanrtk <dahmna@gmail.com>
2020-08-19 16:36:20 +0900

    refresh all files for work

--- main.js
+++ main.js
@@ -1,27 +1,38 @@
-test
+// キーボードの矢印キーで移動させる
+const mover = document.querySelector('.mover');
+// const test = document.getElementById('test');
+
+// 目標位置
+let positionX = 0;
+let positionY = 0;
+
+// 現在位置
+let currentX = 0;
+let currentY = 0;
+
+window.addEventListener('keyup', keymove, false);
+
+tick();
 
 
+function keymove(event) {
+  let key_code = event.keyCode;
+  if(key_code === 37) {
+    positionX -= 32;
+  } else if (key_code === 38) {
+    positionY -= 32;
+  } else if (key_code === 39) {
+    positionX +=32;
+  } else if (key_code === 40) {
+    positionY += 32;
+  }
+}
 
+function tick() {
+  window.requestAnimationFrame(tick);
 
+  currentX += (positionX - currentX) * 0.1;
+  currentY += (positionY - currentY) * 0.1;
 
-<<<<<<< HEAD
-update
-
-
-
-
-
-
-test
-
-
-
-これでどう？
-=======
-これから書き換える
-
-これならどうかな
-
-
-こんな感じ、そんな感じ
->>>>>>> b92c3617e5e853a3a2acfea50face8ee329c9983
+  mover.style.transform = `translate(${currentX}px, ${currentY}px)`;
+};
