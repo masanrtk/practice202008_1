3fdaa96c4e25a5b4708e72a557fdad4fae88d19a
masanrtk <dahmna@gmail.com>
2020-08-20 16:23:49 +0900

    update due to outcome of work in library

--- main.js
+++ main.js
@@ -1,3 +1,40 @@
+const create_floor_btn = document.getElementById("createfloor");
+const floor_area = document.getElementById("floor");
+
+let floor_size = 5;
+
+// createFloor();
+
+create_floor_btn.addEventListener("click", createFloor);
+
+function createFloor() {
+  let i, j;
+  let tr, td;
+
+  let floor_table = document.createElement('table');
+  floor_table.className = "floor_table";
+  floor_table.border = "0";
+
+  for(i = 0; i < floor_size; i++) {
+    tr = floor_table.insertRow(-1);
+    for(j = 0; j < floor_size; j++) {
+      td = tr.insertCell(-1);
+      td.innerHTML = "<img src = \"floor.png\">";
+      td.className = `cell${i}${j}`;
+    }
+  }
+
+  floor_area.innerHTML = "";
+  floor_area.appendChild(floor_table);
+  console.log(floor_area);
+  
+}
+
+
+
+
+
+
 // キーボードの矢印キーで移動させる
 const mover = document.querySelector('.mover');
 // const test = document.getElementById('test');
@@ -12,9 +49,9 @@ let currentY = 0;
 
 const MOVE_UNIT = 64;
 
-window.addEventListener('keyup', keymove, false);
+// window.addEventListener('keyup', keymove, false);
 
-tick();
+// tick();
 
 
 function keymove(event) {
