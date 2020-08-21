012bec8313f82f9f3db9a07b179464a4ffb3915a
masanrtk <dahmna@gmail.com>
2020-08-21 09:35:52 +0900

    floor and object initialization done

--- main.js
+++ main.js
@@ -2,37 +2,85 @@ const create_floor_btn = document.getElementById("createfloor");
 const floor_area = document.getElementById("floor");
 
 let floor_size = 5;
+var floor_containts = []; // リファクタリング時はfloorObjectのプロパティ
+let brave_position_x, brave_position_y;  // リファクタリング時はbraveObjectのプロパティ
 
-// createFloor();
+create_floor_btn.addEventListener("click", newGame);
 
-create_floor_btn.addEventListener("click", createFloor);
+function newGame() {
+  createNewFloor();
+  placeObject('G');
+  placeObject('m');
+  placeObject('b');
+  depictFloorAndObject();
+}
 
-function createFloor() {
-  let i, j;
+/*
+ * リファクタリングのときにObjectにしたときにイベントのコールバックになる？
+ * オブジェクトを引数にする？ floor_containts をどこからもってくるか。
+ * リファクタリング：フロアプラン作成→出口の位置、冒険者の位置、モンスターの位置
+ * 　　　　　　　　　それから、HTML作成で、分離すべきだな
+ */
+function createNewFloor() { // リファクタリング時はfloorObjectのメソッド
+  let x, y;
+  for(y = 0; y < floor_size; y++) {
+    for(x = 0; x < floor_size; x++) {
+      floor_containts[y * floor_size + x] = 'f';
+    }
+  }
+}
+
+function depictFloorAndObject() {
   let tr, td;
+  let x, y;
 
   let floor_table = document.createElement('table');
   floor_table.className = "floor_table";
   floor_table.border = "0";
 
-  for(i = 0; i < floor_size; i++) {
+  for(y = 0; y < floor_size; y++) {
     tr = floor_table.insertRow(-1);
-    for(j = 0; j < floor_size; j++) {
+    for(x = 0; x < floor_size; x++) {
       td = tr.insertCell(-1);
-      td.innerHTML = "<img src = \"floor.png\">";
-      td.className = `cell${i}${j}`;
+      td.className = `cell${y}${x}`;
+      if(floor_containts[y * floor_size + x] === 'f') {
+        td.innerHTML = "<img src = \"floor.png\">";
+      } else if (floor_containts[y * floor_size + x] === 'G') {
+        td.innerHTML = "G";
+      } else if (floor_containts[y * floor_size + x] === 'b') {
+        td.innerHTML = "b";
+      } else if (floor_containts[y * floor_size + x] === 'm') {
+        td.innerHTML = "m";
+      } else {
+        // err
+      }
     }
   }
 
   floor_area.innerHTML = "";
   floor_area.appendChild(floor_table);
   console.log(floor_area);
-  
 }
 
 
+// get Positionと placeは共通化可能
+function placeObject(type) {
+  let x, y;
+  let random;
 
+  do {
+    random = Math.floor(Math.random() * (floor_size * floor_size));
+    y = Math.floor(random / floor_size);
+    console.log(y);
+    x = random - y * floor_size;
+    console.log(x);
+    console.log(floor_containts[y * floor_size + x]);
+//  } while(0);
+  } while (floor_containts[y * floor_size + x] !== 'f');
 
+  floor_containts[y * floor_size + x] = type;
+
+}
 
 
 // キーボードの矢印キーで移動させる
