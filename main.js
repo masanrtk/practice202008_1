d0a5728265161e5586b097d729fafd1e7d23d0fc
masanrtk <dahmna@gmail.com>
2020-08-25 07:11:16 +0900

    just tribial changes

--- main.js
+++ main.js
@@ -316,18 +316,6 @@ function monsterMove() {
   } while(1);
 }
 
-/*
-function bravePosition() { // ｘとｙの値は、brave オブジェクトのプロパティにできる
-  let i;
-  for(i = 0; i < floor_size * floor_size; i++) {
-    if(floor_containts[i] === 'b') {
-      console.log(i);
-      return i;
-    }
-  }
-}
-*/
-
 // 頭悪いよね。場所ぐらいプロパティで記憶しておけばいいのに
 // いなければ -1 を返す
 function getCurrentPosition(type) { // ｘとｙの値は、type オブジェクトのプロパティにできる
@@ -342,59 +330,3 @@ function getCurrentPosition(type) { // ｘとｙの値は、type オブジェク
   return -1; // 見つからなかった
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// キーボードの矢印キーで移動させる
-const mover = document.querySelector('.mover');
-// const test = document.getElementById('test');
-
-// 目標位置
-let positionX = 0;
-let positionY = 0;
-
-// 現在位置
-let currentX = 0;
-let currentY = 0;
-
-const MOVE_UNIT = 64;
-
-// window.addEventListener('keyup', keymove, false);
-
-// tick();
-
-
-function keymove(event) {
-  let key_code = event.keyCode;
-  if(key_code === 37) {
-    positionX -= MOVE_UNIT;
-  } else if (key_code === 38) {
-    positionY -= MOVE_UNIT;
-  } else if (key_code === 39) {
-    positionX += MOVE_UNIT;
-  } else if (key_code === 40) {
-    positionY += MOVE_UNIT;
-  }
-}
-
-function tick() {
-  window.requestAnimationFrame(tick);
-
-  currentX += (positionX - currentX) * 0.1;
-  currentY += (positionY - currentY) * 0.1;
-
-  mover.style.transform = `translate(${currentX}px, ${currentY}px)`;
-};
